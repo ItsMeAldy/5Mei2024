4b9ea877021d7f523f4c985e3c9509354f550549
ItsMeAldy <rizaldimustakim10@gmail.com>
2024-05-02 23:38:39 +0800

    first commit

---
+++ app/not-found.jsx
@@ -0,0 +1,10 @@
+export default function NotFoundPage(){
+    return (
+    <main className="not-found">
+    <h1>
+        Not Found 
+    </h1>
+    <p>unfortunely, we could not find the requested page or resource</p>
+    </main>
+    );
+}
